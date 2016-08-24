6d068ef84c695509347fff4f00a83dceca2cb968
Thang Tong <tongthang0104@gmail.com>
2016-08-23 21:14:02 -0700

    for deploy

--- server/twitterAPI.js
+++ server/twitterAPI.js
@@ -1,11 +1,11 @@
 var Twitter = require('twitter');
-var config = require('./env/config.js');
+// var config = require('./env/config.js');
 
 var client = new Twitter({
-      consumer_key: config.TWITTER_CONSUMER_KEY,
-      consumer_secret: config.TWITTER_CONSUMER_SECRET,
-      access_token_key: config.TWITTER_ACCESS_TOKEN,
-      access_token_secret: config.TWITTER_ACCESS_TOKEN_SECRET
+      consumer_key: process.env.TWITTER_CONSUMER_KEY ,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 exports.getFollow = function(path, params, callback) {
